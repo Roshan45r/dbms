231dcde9e05a099337c44ab2498ced9b1dcb0af1
Roshan <84079544+Roshan45r@users.noreply.github.com>
2022-07-31 14:45:53 +0530

    Update banking.sql

--- banking.sql
+++ banking.sql
@@ -1,84 +1,69 @@
-create database banking;
-use banking;
+CREATE TABLE branch(branch_name char(30), branch_city char(30), assets real, PRIMARY KEY(branch_name));
+CREATE TABLE bank_account(accno int, branch_name char(30), balance real, PRIMARY KEY(accno), FOREIGN KEY(branch_name) REFERENCES branch(branch_name));
+CREATE TABLE bank_customer(customer_name char(30), customer_street char(30), customer_city char(30));
+CREATE TABLE depositor(customer_name char(30), accno int, FOREIGN KEY(accno) REFERENCES bank_account(accno));
+CREATE TABLE loan(loan_number int, branch_name char(30), amount real, PRIMARY KEY(loan_number), FOREIGN KEY(branch_name) REFERENCES branch(branch_name));
 
-create table branch(
-	branch_name varchar(40),
-    branch_city varchar(40),
-    assets double,
-    primary key(branch_name)
-    );
-    
-create table accounts(
-	accno int,
-    branch_name varchar(40),
-    balance double,
-    primary key(accno),
-    foreign key(branch_name) references branch(branch_name)
-    );
-    
-create table customer(
-	customername varchar(40),
-    customerstreet varchar(40),
-    city varchar(40),
-    primary key(customername)
-    );
-    
-create table depositor(
-	customername varchar(20),
-    accno int,
-    primary key(customername,accno),
-    foreign key(customername) references customer(customername),
-    foreign key(accno) references accounts(accno)
-    );
-    
-create table loan(
-	loannumber int,
-    branch_name varchar(40),
-    ammount int,
-    primary key(loannumber),
-    foreign key(branch_name) references branch(branch_name)
-);
+INSERT INTO branch values('SBI_Basvangudi', 'Bangalore', 50000);
+INSERT INTO branch values('BOI_Sakchit', 'Jamshedpur', 2000);
+INSERT INTO branch values('SBI_Chamrajpet', 'Bangalore', 900000);
+INSERT INTO branch values('HDFC_Tirupathur', 'Vellore', 6000);
+INSERT INTO branch values('PNB_Banashankari', 'Bangalore', 10000);
+Commit;
+SELECT * FROM branch;
 
-create table borrower(
-	customername varchar(40),
-    loannumber int,
-    primary key(customername,loannumber),
-    foreign key(customername) references customer(customername),
-    foreign key(loannumber) references loan(loannumber)
-    );
-    
-insert into branch values('bavangudi','bangalore',123693);
-insert into branch values('hanumanthnagar','delhi',1265693.26);
-insert into branch values('nr colony','bangalore',963693.83);
-select * from branch;
+INSERT INTO loan values(1,'SBI_Basvangudi', 2000);
+INSERT INTO loan values (3,'BOI_Sakchit', 100000);
+INSERT INTO loan VALUES (2, 'SBI_Chamrajpet', 300);
+INSERT INTO loan VALUES (4, 'HDFC_Tirupathur', 500);
+INSERT INTO loan VALUES (5, 'PNB_Banashankari', 40000);
+Commit;
+SELETCT * FROM loan;
 
-insert into accounts values(1001,'bavangudi',1000);
-insert into accounts values(1002,'nr colony',2000);
-insert into accounts values(1003,'nr colony',2500);
-select * from accounts;
-
-insert into customer values('sanju','mg road','bangalore');
-insert into customer values('roshan','nr colony','hubli');
-insert into customer values('sai','nr colony','mysore');
-select * from customer;
-
-insert into depositor values('sanju',1001);
-insert into depositor values('roshan',1002);
-insert into depositor values('sai',1003);
-select * from depositor;
-
-insert into loan values(100,'bavangudi',100000);
-insert into loan values(101,'bavangudi',20000);
-insert into loan values(102,'hanumanthnagar',56916);
-select * from loan;
-
-insert into borrower values('sanju',100);
-insert into borrower values('roshan',101);
-insert into borrower values('roshan',102);
-select * from borrower;
-
-commit;
+INSERT INTO bank_account VALUES(12, 'BOI_Sakchit', 1230);
+INSERT INTO bank_account VALUES(23, 'HDFC_Tirupathur', 4220);
+INSERT INTO bank_account VALUES(51, 'PNB_Banashankari', 9820);
+INSERT INTO bank_account VALUES(109, 'SBI_Chamrajpet', 14000);
+INSERT INTO bank_account VALUES(84, 'SBI_Basvangudi', 121); 
+INSERT INTO bank_account VALUES(123, 'SBI_Basvangudi', 13120);
+INSERT INTO bank_account VALUES(13, 'SBI_Basvangudi', 2120);
+INSERT INTO bank_account VALUES(902, 'SBI_Basvangudi', 13120);
+INSERT INTO bank_account VALUES(832, 'SBI_Basvangudi', 13120);
+INSERT INTO bank_account VALUES(119,"SBI_Chamrajpet",12412);
+INSERT INTO bank_account VALUES(39,"HDFC_Tirupathur",1221);
+INSERT INTO bank_account VALUES(190,"BOI_Sakchit",1212412);
+Commit;
+SELECT * FROM bank_account;
 
 
 
+INSERT INTO bank_customer VALUES("Raju", "MG","Bangalore");
+INSERT INTO bank_customer VALUES("Shashank","Jehangir","Ranchi");
+INSERT INTO bank_customer VALUES("Abishek","Marine Drive","Delhi");
+INSERT INTO bank_customer VALUES("Diwakar","14th Milestone","Jamshedpur");
+INSERT INTO bank_customer VALUES("Sid","Teram","Vellore");
+Commit;
+Select * from bank_customer;
 
+INSERT INTO depositor VALUES("Raju",12);
+INSERT INTO depositor VALUES("Shashank",23);
+INSERT INTO depositor VALUES("Abishek",51);
+INSERT INTO depositor VALUES("Diwakar",109);
+INSERT INTO depositor VALUES("Sid",84);
+INSERT INTO depositor VALUES("Raju",123);
+INSERT INTO depositor VALUES("Raju",832);
+INSERT INTO depositor VALUES("Abishek",902);
+INSERT INTO depositor VALUES("Abishek",39)
+INSERT INTO depositor VALUES("Abishek",190)
+INSERT INTO depositor VALUES("Abishek",119)
+
+
+Commit;
+
+Select * from depositor;
+
+Queries: 
+3) SELECT customer_name FROM depositor d, bank_account a where d.accno=a.accno AND a.branch_name="SBI_Basvangudi" GROUP BY d.customer_name HAVING COUNT(d.customer_name)>=2;
+4)
+SELECT d.customer_name FROM bank_account a,branch b,depositor d WHERE b.branch_name=a.branch_name AND  a.accno=d.accno AND b.branch_city='Bangalore' GROUP BY d.customer_name HAVING COUNT(distinct b.branch_name)=(SELECT COUNT(branch_name) FROM branch WHERE branch_city='Bangalore');
+5)  DELETE FROM bank_account WHERE branch_name IN(SELECT branch_name FROM branch WHERE branch_city='Jamshedpur');
